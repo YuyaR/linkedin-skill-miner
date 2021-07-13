02509fad147e83bd96c49e770f6631b2eb26b412
yuyacademia@gmail.com <yuyacademia@gmail.com>
2021-07-13 20:51:51 +0900

    text_mining.py can find descriptions of jobs

--- skill_mining.py
+++ skill_mining.py
@@ -1,4 +1,27 @@
 import pandas as pd
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from bs4 import BeautifulSoup
+from bs4 import SoupStrainer
+import requests
 
-pd.read_csv('/Users/yuyara/Documents/Coding/python/Data_pipeline/Career_skill/job_data.csv')
+DF = pd.read_csv('/Users/yuyara/Documents/Coding/python/Data_pipeline/Career_skill/Data/job_data.csv')
+links = list(DF['Link'])
+options = Options()
+options.headless = False
+driver = webdriver.Chrome(options=options, executable_path="/Users/yuyara/Downloads/chromedriver 2") 
+
+
+
+bunch_texts = []
+for url in links:
+    driver.get(url)
+    try:
+        main = driver.find_element_by_id("main-content")
+        texts = main.find_element_by_class_name('show-more-less-html__markup')
+        text = texts.get_attribute('innerHTML')
+        bunch_texts.append(text)
+    except:
+        bunch_texts.append('Null')
+
 
