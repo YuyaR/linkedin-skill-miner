ad46b80060351c610bf8f2a2a4b1ba0aefd66849
yuyacademia@gmail.com <yuyacademia@gmail.com>
2021-07-23 19:43:05 +0900

    can count words now including wild card search

--- link_scraper.py
+++ link_scraper.py
@@ -55,9 +55,6 @@ raw_data = pd.DataFrame(list(zip(title, employee, link)), columns=['Title', 'Emp
 
 DF = raw_data.drop_duplicates(['Title','Employee'])
 
+DF[:10]
+
 DF.to_csv('/Users/yuyara/Documents/Coding/python/Data_pipeline/Career_skill/job_data.csv', index=False, header=True)
-
-
-
-
-
--- skill_mining.py
+++ skill_mining.py
@@ -4,24 +4,42 @@ from selenium.webdriver.chrome.options import Options
 from bs4 import BeautifulSoup
 from bs4 import SoupStrainer
 import requests
+import fnmatch
 
 DF = pd.read_csv('/Users/yuyara/Documents/Coding/python/Data_pipeline/Career_skill/Data/job_data.csv')
 links = list(DF['Link'])
 options = Options()
-options.headless = False
+options.headless = True
 driver = webdriver.Chrome(options=options, executable_path="/Users/yuyara/Downloads/chromedriver 2") 
 
-
-
-bunch_texts = []
+bullets = []
 for url in links:
     driver.get(url)
     try:
-        main = driver.find_element_by_id("main-content")
-        texts = main.find_element_by_class_name('show-more-less-html__markup')
-        text = texts.get_attribute('innerHTML')
-        bunch_texts.append(text)
+        # main = driver.find_element_by_id("main-content")
+        main = driver.find_element_by_xpath("//*[@class = 'show-more-less-html__markup show-more-less-html__markup--clamp-after-5']")
+        # text = texts.get_attribute('innerHTML')
+        # bunch_texts.append(text)
+        bp = main.find_elements_by_xpath('.//ul')
+        for n in bp:
+            tx = n.get_attribute('innerText')
+            txt = tx.split('\n')
+        bullets.extend(txt)
     except:
-        bunch_texts.append('Null')
+        bullets.append(None)
 
+final_list = [i for i in bullets if i] #removing empty strings
+
+#now text mining
+words = []
+for s in final_list:
+    sentence = s.split(' ')
+    sentence = [i.lower() for i in sentence]
+    words.extend(sentence)
+
+keywords = {'planning': None, 'communication': None, 'analy': None} #not sure how to do a wild card thing like analy*
+
+for kw in keywords.keys():
+    word_count = len(fnmatch.filter(words, f'{kw}*'))
+    keywords[kw] = word_count
 
