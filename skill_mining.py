6633e4c4b7b2963b6745ec1d06b52527fd9eefc5
yuyacademia@gmail.com <yuyacademia@gmail.com>
2021-08-19 22:54:10 +0900

    added description skill miner

--- skill_mining.py
+++ skill_mining.py
@@ -2,25 +2,41 @@ import pandas as pd
 from link_scraper import LinkScraper
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+import fnmatch
+import matplotlib.pyplot as plt
 # from bs4 import BeautifulSoup
 # from bs4 import SoupStrainer
 # import requests
 # from requests_html import HTMLSession
-import fnmatch
-import matplotlib.pyplot as plt
 
 #TODO: make import-all file
 #TODO: see if can skip creating the data frame
 
 class TextMiner(LinkScraper):
+    '''
+    This class inherits the following attributes from LinkScraper:
 
+    Attributes:
+        job: type of job for search (case-insensitive)
+        location: the location where the jobs are posted (case-insensitive)
+        chrome_path: the path to Chrome Driver on your device
+
+    It contains methods that scrape the bulletpoints from each job listing 
+    collected from Linkedin, then count the number of occurrence for ten
+    most common transferable skills. 
+    The result is displayed by a barplot.
+    '''
     DF = pd.read_csv('./job_data.csv')
 
     def getText(self, n=len(DF)):
         '''
-        Args:
-        n: the number of links/jobs to get information from (if list too long, 
-            user may consider selecting only first n jobs). Default is whole list.
+        This function uses selenium to scrape all bullet points from each job listing (from links)
+
+        Arguments:
+            n: the number of links/jobs to get information from (if list too long, 
+                user may consider selecting only first n jobs). Default is whole list.
+        Returns:
+            a barplot displaying the resulting count for each skill
         '''
 
         links = list(DF['Link'][:n])
@@ -61,6 +77,13 @@ class TextMiner(LinkScraper):
         self.plot(keywords)
 
     def mineText(self, final_list):
+        '''
+        This method counts how many times each of the skill keyword in the keywords dictionary
+        is found in the scraped bullet points
+
+        Returns: 
+            a dictionary with the skill and its number of occurrence
+        '''
         words = []
         for s in all_text:
             sentence = s.split(' ')
@@ -69,7 +92,7 @@ class TextMiner(LinkScraper):
 
         global keywords
 
-        keywords = {'planning': None, 'communication': None, 'analy': None, 'organi': None,
+        keywords = {'plan': None, 'communicat': None, 'analy': None, 'organi': None,
                     'independen': None, 'creativ': None, 'collabor': None, 'manage': None, 
                     'initiat': None, 'lead': None}
 
@@ -81,9 +104,12 @@ class TextMiner(LinkScraper):
 
     @staticmethod
     def plot(dic):
+        '''
+        a static method visualising the frequency of occurrence of each skill in a barplot.
+        '''
         skills = ['planning', 'communication', 'analysis', 'organisation', 'independence', 'creativity', 'collaboration', 'management', 'initiative', 'leadership']
         plt.bar(x=skills, height=dic.values())
-        plt.title('How the Top Transferable Skills are Desired in Your Dream Job!')
+        plt.title('How the Top Transferable Skills are Desired in Your Dream Job')
         plt.show()
 
     # if __name__ == '__main__':
