a7a081c90aeca6d434f960787258e1cff2420e98
yuyacademia@gmail.com <yuyacademia@gmail.com>
2021-08-23 16:49:28 +0100

    published app

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup
-from setuptools import find_packages()
+from setuptools import find_packages
 
 setup(
     name='careerskills',
     version='0.0.1',
-    description='A graphic user interface that allows you to find out the top desired transferable skills in your dream career'
-    url='https://github.com/YuyaR/linkedin-skill-miner.git'
+    description='A graphic user interface that allows you to find out the top desired transferable skills in your dream career',
+    url='https://github.com/YuyaR/linkedin-skill-miner.git',
     author='Yuya Ra',
     License='MIT',
     packages=find_packages(),
