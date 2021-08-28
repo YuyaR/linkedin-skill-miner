f2e75aa3e91475717cb50d620afdd97a21088eb7
yuyacademia@gmail.com <yuyacademia@gmail.com>
2021-08-28 21:29:17 +0100

    created aws rds class with two methods

--- aws_rds/aws_rds.py
+++ aws_rds/aws_rds.py
@@ -2,21 +2,24 @@ from sqlalchemy import create_engine
 import pandas as pd
 
 class AwsSQL:
-    def __init__(self, DATABASE_TYPE, DBAPI, ENDPOINT, USER, PASSWORD, PORT, DATABASE):
+    def __init__(self):
+        DATABASE_TYPE = 'postgresql'
+        DBAPI = 'psycopg2'
+        ENDPOINT = 'career-skills.c6a6xhdjmptr.ap-northeast-1.rds.amazonaws.com'
+        USER = 'postgres'
+        PASSWORD = 'c8ydw9Ar8opmK8IRh78C'
+        PORT = 5432
+        DATABASE = 'postgres'
+
         self.engine = create_engine(f"{DATABASE_TYPE}+{DBAPI}://{USER}:{PASSWORD}@{ENDPOINT}:{PORT}/{DATABASE}")
 
     def save_dataset(self, df):
         df.to_sql(f'{self.job}{self.loc}_skills', self.engine, if_exists='replace')
 
+    def read_table(self, tbl):
+        self.engine.execute(f'''SELECT * FROM {tbl}
+            LIMIT 20''').fetchall()
 
-if __name__ == '__main__':
-    DATABASE_TYPE = 'postgresql'
-    DBAPI = 'psycopg2'
-    ENDPOINT = 'career-skills.c6a6xhdjmptr.ap-northeast-1.rds.amazonaws.com'
-    USER = 'postgres'
-    PASSWORD = 'c8ydw9Ar8opmK8IRh78C'
-    PORT = 5432
-    DATABASE = 'postgres'
 
 
     
