be2bbe0c770db8a9a605fe19dc3fa5bf854715f7
yuyacademia@gmail.com <yuyacademia@gmail.com>
2021-10-03 20:15:54 +0100

    changed setup.py to prepare for pypi publish

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import find_packages
 
 setup(
     name='careerskills',
-    version='0.0.7',
-    description='A graphic user interface that allows you to find out the top desired transferable skills in your dream career',
+    version='0.1.0',
+    description='A graphic user interface that allows you to find out the top desired transferable skills in your dream career; can be run in docker and upload results to AWS postgreSQL database',
     url='https://github.com/YuyaR/linkedin-skill-miner.git',
     author='Yuya Ra',
     author_email='yuyacademia@gmail.com',
